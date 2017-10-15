cf5b7acdd50e724b3320c0a87ae82bd387678725
Tobias Steppan <t.steppan@googlemail.com>
2017-10-15 10:52:07 +0200

    finished exercise #2

--- exercise-2/ASCII/main.cpp
+++ exercise-2/ASCII/main.cpp
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
@@ -59,6 +60,50 @@ void AsciiTabelleKomplett()
         }
         cout<<endl;//nächste zeile
     }
+    cout<<endl;
+}
+
+void RundungsfehlerDurchFortgesetzteMultiplikation()
+{
+    /*  description:    Aufgabe 2.2 float x=10^6 10 mal mit float k=0.1693 multiplizieren, danach 10 mal durch k dividieren um gleichheit zu überprüfen
+        parameters:     n/a
+        return value:   n/a
+    */
+    float x = 1000000;
+    float k = 0.1693;
+    for(int i=0; i<10; i++) //zehnfache multiplikation
+    {
+        x=k*x;
+    }
+    cout<<setprecision(9)<<"Ergebnis nach zehnfacher Multiplikation: "<<x<<endl;
+    for(int i=0; i<10; i++)
+    {
+        x=x/k;
+    }
+    cout<<setprecision(9)<<"Ergebnis nach zehnfacher Division: "<<x<<endl;
+    float result = 1000000-x;
+    cout<<setprecision(9)<<"Entstandener Rundungsfehler: "<<result<<endl<<endl;
+}
+
+void FlaechenberechnungDreieck(float a, float b, float c)
+{
+    /*  description:    flaecheninhalt berechnen (formel 2.1 und 2.2), diskriminante auf negativität prüfen, ergebnis ausgeben
+        parameters:     Kanten eines Dreiecks
+        return value:   n/a
+    */
+    float result = 0.0;
+    float s = 0.0;
+    s=(a+b+c)/2.0; //größe für formel 1, halber umfang
+    if((s*(s-a)*(s-b)*(s-c))<0)
+    {
+        cout<<"Die Eingabewerte entsprechen nicht denen eines Dreiecks"<<endl<<endl;
+    }else
+    {
+        result = sqrt(s*(s-a)*(s-b)*(s-c)); //formel 2.1
+        cout<<"Der Flaecheninhalt nach Formel 2.1 betraegt: " <<result<<endl<<endl;
+        result=(1.0/4.0)*sqrt((a+b+c)*(a+b-c)*(b+c-a)*(c+a-b)); //formel 2.2
+        cout<<"Der Flaecheninhalt nach Formel 2.2 betraegt: " <<result<<endl<<endl;
+    }
 }
 
 int main()
@@ -67,8 +112,25 @@ int main()
     parameters:
     return value:
 */
+    //Aufgabe 2.1
     cout<<"Aufgabe 2.1:"<<endl<<endl;
     TabelleMitGrundstruktur();
     AsciiTabelleKomplett();
+    //Aufgabe 2.2
+    cout<<endl<<"Aufgabe 2.2: Rundungsfehler:"<<endl;
+    RundungsfehlerDurchFortgesetzteMultiplikation();
+    //Aufgabe 2.3
+    float a=0.0;
+    float b=0.0;
+    float c=0.0;
+    cout<<"Aufgabe 2.3 Flaeche eines Dreiecks: "<<endl;
+    cout<<"Dieses Programm errechnet den Flaecheninhalt eines Dreiecks anhand dessen Kanten a, b und c"<<endl<<"Bitte geben Sie Kantenlaenge a ein:"<<endl;
+    cin>>a;
+    cout<<"Bitte geben Sie Kantenlaenge b ein:"<<endl;
+    cin>>b;
+    cout<<"Bitte geben Sie Kantenlaenge c ein:"<<endl;
+    cin>>c;
+    cout<<"Sie haben die Kantenlaengen a="<<a<<", b="<<b<<" und c="<<c<<" eingegeben"<<endl;
+    FlaechenberechnungDreieck(a,b,c);
     return 0;
 }
