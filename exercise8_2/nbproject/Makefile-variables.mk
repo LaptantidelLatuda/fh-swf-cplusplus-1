e4305931a040a2d9a1ea19aed31fd49294a63a38
Tobias Steppan <t.steppan@googlemail.com>
2018-03-31 14:32:48 +0200

    clean

--- exercise8_2/nbproject/Makefile-variables.mk
+++ exercise8_2/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_1-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_1-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=exercise8_2
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_1-Windows/exercise8_2
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_1-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/exercise8_2
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=exercise82.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_1-Windows/package/exercise82.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/exercise82.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_1-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_1-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=exercise8_2
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_1-Windows/exercise8_2
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_1-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/exercise8_2
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=exercise82.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_1-Windows/package/exercise82.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/exercise82.tar
 #
 # include compiler specific variables
 #
